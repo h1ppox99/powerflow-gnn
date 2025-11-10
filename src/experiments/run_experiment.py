4d5de24ddb7b7e7a2914812c6836a51619b2fa41
edouard-rabasse <rabasse.edouard@gmail.com>
2025-11-09 21:59:40 -0800

    feat(models): implement load_model function for PNA and GraphSAGE

--- src/experiments/run_experiment.py
+++ src/experiments/run_experiment.py
@@ -4,6 +4,7 @@ import argparse, yaml
 from src.models.graphsage_pi import GraphSAGE_PI
 from src.training.train import fit
 from pathlib import Path
+from src.models import load_model
 
 def load_dataset(cfg):
     if cfg["data"]["backend"] == "synthetic":
@@ -26,33 +27,7 @@ def load_dataset(cfg):
             datatype=cfg["data"].get("task", "nodeopf")
         )
 
-def load_model(cfg: dict, dataset):
-    """Load the model based on the configuration and dataset.
 
-    Args:
-        cfg (dict): Configuration dictionary.
-        dataset (InMemoryDataset): The dataset to be used for training. 
-
-    Raises:
-        ValueError: If the model name is unknown.
-
-    Returns:
-        _type_: torch.nn.Module instance of the model.
-    """
-    if cfg["model"]["name"] == "graphsage_pi":
-        data = dataset[0]  # Assuming dataset is an InMemoryDataset
-        in_dim = data.x.size(-1)
-        out_dim = data.y.size(-1)
-        return GraphSAGE_PI(
-            in_dim=in_dim,
-            hidden_dim=cfg["model"]["hidden"],
-            out_dim=out_dim,
-            num_layers=cfg["model"]["num_layers"],
-            dropout=cfg["model"]["dropout"],
-            aggr=cfg["model"]["agg"]
-        )
-    else:
-        raise ValueError(f"Unknown model name: {cfg['model']['name']}")
 
 def main():
     ap = argparse.ArgumentParser()
